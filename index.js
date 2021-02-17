322e192372ba750c44c572073f2312bbda903525
Sean <skabe89@gmail.com>
2021-02-16 21:20:37 -0500

    commit tree ac08286e9073e2cb3ea890a3343dbb8c4bb4f866

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Add your code here
+
+function submitData(name, email){
+  
+   let userInfo =  {
+      "name": name,
+      "email": email
+    }
+  
+  let configObj = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify(userInfo)
+  }
+  return fetch("http://localhost:3000/users", configObj)
+  .then(resp => resp.json())
+  .then(json => renderUser(json.id))
+  .catch(function(error) {
+    document.body.innerHTML = "Unauthorized Access"
+    console.log(error)
+  })
+}
+
+function renderUser(info){
+  console.log(info)
+  document.body.innerHTML = info
+}
